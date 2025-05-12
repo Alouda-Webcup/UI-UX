c9615b4dd9a7557c450ffec2356ba5bb3af4896e
Karenathi <hei.karenandriantasy@gmail.com>
2025-05-12 14:20:59 +0400

    initial commit

---
+++ config/bdconnect.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost"; 
+$dbname = "alouda_db";
+$username = "alouda_db";
+$password = "dacram-nIpqes-xinji9";
+
+// Connexion avec PDO
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie";
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
