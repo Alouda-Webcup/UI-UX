630674f457cb70b94ac18bd66f91147f847509da
taw3ii <carmenshumyune@gmail.com>
2025-05-17 19:15:04 +0400

    feat: home

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+    <title>Accueil - TheEnd.page</title>
+</head>
+<body>
+<?php
+include('includes/navbar.php');
+?>
+
+<br><br><br><br>
+<section id="home">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="row text-center">
+                <h1>Parce que chaque fin mérite un dernier mot.</h1>
+                <p>Ici, on ne tourne pas la page.</p>
+                <p>On la brûle, on la customise, on la partage.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="howitworks">
+
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
