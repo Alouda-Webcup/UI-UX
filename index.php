f1690d3d973ff13b723c1b2e364815acf530e71f
taw3ii <carmenshumyune@gmail.com>
2025-05-18 08:41:54 +0400

    feat: zoomin

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Accueil</title>
- 
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap' rel='stylesheet'>
-  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
- 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
- 
- 
-  <link rel="stylesheet" href="assets/css/index.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Accueil</title>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
+    
+    
+    <link rel="stylesheet" href="assets/css/index.css"> 
 </head>
  
  
@@ -24,10 +24,10 @@
   <?php include('includes/navbar.php') ?>
  
   <!-- Hero -->
-  <section class="hero fade-in delay-1">
+  <section class="hero zoom-in">
     <h1 class="display-5 fw-bold" id="title1">Parce que chaque fin<br>mérite un dernier mot.</h1>
-    <p class="lead mt-3 delay-2 fade-in">Ici, on ne tourne pas la page...<br>On la <b>brûle</b>, on la <b>customise</b>, et on la <b>partage</b>.</p>
-    <a href="auth/login.php" class="btn btn-dark btn-lg mt-3 rounded-pill fade-in delay-3">Créer une page</a>
+    <p class="lead mt-3 delay-2">Ici, on ne tourne pas la page...<br>On la <b>brûle</b>, on la <b>customise</b>, et on la <b>partage</b>.</p>
+    <a href="auth/login.php" class="btn btn-dark btn-lg mt-3 rounded-pill">Créer une page</a>
     <div class="d-flex flex-wrap justify-content-center gap-3 mt-4">
       <small>✨ Une carte rapide pour chacun</small>
       <small>⚡ En ligne en quelques clics</small>
@@ -42,7 +42,7 @@
   </section>
  
  
-  <!-- Features -->
+  <!-- Mini Tutoriel -->
   <section class="features container">
   <div class="row" data-aos="fade-up" data-aos-delay="300">  
     <h6 class="text-danger mb-2">Comment fonctionne TheEnd ?</h6>
@@ -73,34 +73,35 @@
     </div>
   </section>
  
-  <!-- Section 1 -->
+  <!-- Section Presentation -->
   <section class="section-highlight container fade-in delay-1">
     <div class="row">
-      <div class="col-md-6">
+        <div class="col-md-6" data-aos="fade-up" data-aos-delay="400">
         <img src="assets/img/palmsupguy.png">
       </div>
-      <div class="col-md-6" data-aos="fade-up" data-aos-delay="400">
-        <br>
+
+        <div class="col-md-6" data-aos="fade-up" data-aos-delay="400">
+            <br>
         <p class="text-orange">C’est la fin ? Fête ça dignement.</p>
         <h3 class="fw-bold" id="title2">Ne pars pas sans dire un dernier mot.</h3>
-        <p class="mt-3">TheEnd.page t’aide avec une plateforme pour créer ta page de départ pleine de style (ou de larmes).</p>
+        <p class="mt-3">TheEnd.page t’aide pour créer ta page de départ pleine de style (ou de larmes).</p>
         <br>
         <a href="auth/login.php" class="btn-custom">Créer une page</a>
       </div>
     </div>
   </section>
  
-  <!-- Section 2 -->
+  <!-- Section Pourquoi -->
   <section class="section-highlight container fade-in delay-1">
     <div class="row flex-md-row-reverse">
-      <div class="col-md-6" data-aos="fade-in" data-aos-delay="200">
+      <div class="col-md-6" data-aos="fade-up" data-aos-delay="400">
         <img src="assets/img/palmsupgirl.png">
       </div>
       <div class="col-md-6" data-aos="fade-up" data-aos-delay="400">
         <br>
         <p class="text-orange">C’est la fin ? Fête ça dignement.</p>
-        <h3 class="fw-bold" id="title2">Pourquoi faire ça ?</h3>
-        <p class="mt-3">Parce que parfois, une fin ça peut aussi faire du bien. Parce qu'il faut laisser une trace de tout ce qu'on a perdu.</p>
+        <h3 class="fw-bold" id="title2">Pourquoi utiliser TheEnd.page ?</h3>
+        <p class="mt-3">Parce que parfois, une fin ça peut aussi faire du bien. <br> Ou parce qu'il faut laisser une trace de tout ce qu'on a perdu.</p>
         <br>
         <a href="auth/login.php" class="btn-custom">Créer une page</a>
       </div>
