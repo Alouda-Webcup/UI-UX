5de559fb50ca43d7a5982bcd92654a3f8d8fddb0
taw3ii <carmenshumyune@gmail.com>
2025-05-18 06:49:48 +0400

    feat: index animations

--- index.php
+++ index.php
@@ -11,19 +11,23 @@
   <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
+
 
   <link rel="stylesheet" href="assets/css/index.css">
 </head>
+
+
 <body>
 
   <!-- Header -->
   <?php include('includes/navbar.php') ?>
 
   <!-- Hero -->
-  <section class="hero">
-    <h1 class="display-5 fw-bold" id="title">Parce que chaque fin<br>mérite un dernier mot.</h1>
-    <p class="lead mt-3">Ici, on ne tourne pas la page...<br>On la <b>brûle</b>, on la <b>customise</b>, et on la <b>partage</b>.</p>
-    <a href="auth/login.php" class="btn btn-dark btn-lg mt-3 rounded-pill">Créer une page</a>
+  <section class="hero fade-in delay-1">
+    <h1 class="display-5 fw-bold" id="title1">Parce que chaque fin<br>mérite un dernier mot.</h1>
+    <p class="lead mt-3 delay-2 fade-in">Ici, on ne tourne pas la page...<br>On la <b>brûle</b>, on la <b>customise</b>, et on la <b>partage</b>.</p>
+    <a href="auth/login.php" class="btn btn-dark btn-lg mt-3 rounded-pill fade-in delay-3">Créer une page</a>
     <div class="d-flex flex-wrap justify-content-center gap-3 mt-4">
       <small>✨ Une carte rapide pour chacun</small>
       <small>⚡ En ligne en quelques clics</small>
@@ -31,17 +35,21 @@
     </div>
   </section>
 
+
   <!-- Gradient Notes Section -->
   <section class="gradient-section container">
-    <div class="row gy-4">
-    </div>
+    <div class="row gy-4"></div>
   </section>
+
 
   <!-- Features -->
   <section class="features container">
+  <div class="row" data-aos="fade-up" data-aos-delay="900">  
     <h6 class="text-danger mb-2">Comment fonctionne TheEnd ?</h6>
-    <h2 class="fw-bold mb-5" id="title">Tu quittes quelque chose ou quelqu’un ?<br>Fais-le avec style.</h2>
-    <div class="row text-center">
+    <h2 class="fw-bold mb-5" id="title2">Tu quittes quelque chose ou quelqu’un ?<br>Fais-le avec style.</h2>
+  </div>
+
+    <div class="row text-center" data-aos="fade-up" data-aos-delay="900">
       <div class="col-md-3">
         <img src="assets/img/palette.png" class="img-fluid w-25">
         <p><b>Choisis ton style</b></p>
@@ -66,14 +74,14 @@
   </section>
 
   <!-- Section 1 -->
-  <section class="section-highlight container">
+  <section class="section-highlight container fade-in delay-1">
     <div class="row">
       <div class="col-md-6">
-        <img src="assets/img/palmsupguy.png" alt="Homme">
+        <img src="assets/img/palmsupguy.png">
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" data-aos="fade-up" data-aos-delay="900">
         <p class="text-orange">C’est la fin ? Fête ça dignement.</p>
-        <h3 class="fw-bold" id="title">Ne pars pas sans dire un dernier mot.</h3>
+        <h3 class="fw-bold" id="title2">Ne pars pas sans dire un dernier mot.</h3>
         <p class="mt-3">TheEnd t’aide avec une plateforme pour créer ta page de départ pleine de style (ou de larmes).</p>
         <a href="auth/login.php" class="btn-custom">Créer une page</a>
       </div>
@@ -81,19 +89,27 @@
   </section>
 
   <!-- Section 2 -->
-  <section class="section-highlight container">
+  <section class="section-highlight container fade-in delay-1">
     <div class="row flex-md-row-reverse">
       <div class="col-md-6">
-        <img src="assets/img/palmsupgirl.png" alt="Femme">
+        <img src="assets/img/palmsupgirl.png">
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" data-aos="fade-up" data-aos-delay="900">
         <p class="text-orange">C’est la fin ? Fête ça dignement.</p>
-        <h3 class="fw-bold" id="title">Pourquoi faire ça ?</h3>
-        <p class="mt-3" id="content">Parce que parfois, une fin ça peut aussi faire du bien. Parce que cette vérité-là on la “partage confidentiellement”.</p>
+        <h3 class="fw-bold" id="title2">Pourquoi faire ça ?</h3>
+        <p class="mt-3">Parce que parfois, une fin ça peut aussi faire du bien. Parce qu'il faut laisser une trace de tout ce qu'on a perdu.</p>
         <a href="auth/login.php" class="btn-custom">Créer une page</a>
       </div>
     </div>
   </section>
+
+  <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+<script>
+  AOS.init({
+    once: true
+  });
+</script>
+
 
 </body>
 </html>
